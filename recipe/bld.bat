30387fcaeb7e83ec6448a8e8784440565d0f8f95
Bas Nijholt <basnijholt@gmail.com>
2017-05-15 17:34:20 +0200

    Try building for Windows

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+CD src
+
+copy %PREFIX%\Makefile.inc.i686_pc_mingw32 Makefile.inc
+
+nmake esmumps
+nmake check
+
+CD ..
+
+if not exist "%PREFIX%\lib\" mkdir %PREFIX%\lib\
+if not exist "%PREFIX%\bin\" mkdir %PREFIX%\bin\
+if not exist "%PREFIX%\include\" mkdir %PREFIX%\include\
+
+copy lib\* %PREFIX%\lib\
+copy bin\* %PREFIX%\bin\
+copy include\* %PREFIX%\include\
